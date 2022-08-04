43a2b1725a9beb14edfa52a5a61bd1107080f1b2
scaleoutSean <scaleoutsean@users.noreply.github.com>
2022-08-04 19:53:27 +0800

    Update Volume-to-Node mapping script (PS) to add Sum of Min QoS per node

--- scripts/sfvid2nid.ps1
+++ scripts/sfvid2nid.ps1
@@ -4,45 +4,98 @@
 # sfvid2nid.ps1                                                               #
 #                                                                             #
 # Synopsis:                                                                   #
-# Maps SolidFire volume IDs to storage node IDs                               #
-# Can be useful for optimizing parallel execution of backup and clone jobs    #
+# Maps SolidFire volume IDs to storage node IDs and calculates MinIOPS/Node   #
 #                                                                             #
 # NOTE: uses non-public report API which is may be deprecated without notice  #
-#       and should not be frequently executed (once every 5-10 minutes is OK?)#
+#       and should not be frequently executed (once every 5-10 minutes is OK) #
 #                                                                             #
 # Author: @scaleoutSean                                                       #
 # https://github.com/scaleoutsean/awesome-solidfire                           #
 # License: the BSD License 3.0                                                #
 ###############################################################################
 
+# Import SolidFire module for older PowerShell 5.1
 # Import-Module SolidFire
 
 Import-Module SolidFire.Core
 $null = Connect-SFCluster 192.168.1.30 -Username admin -Password admin
 
-$res = Invoke-SFApi -Method "GetReport" -Params @{"reportName"="slices.json"}
+$res = Invoke-SFApi -Method 'GetReport' -Params @{'reportName' = 'slices.json' }
 
 $vid2nid = @{}
 $service = @{}
+$nodeGuarantee = @{}
+$vMinSum = 0
 
-foreach ($p2n in $res.services) {$service.Add($p2n.serviceID,$p2n.nodeID)}
+foreach ($p2n in $res.services) { $service.Add($p2n.serviceID, $p2n.nodeID) }
+foreach ($n in $service.Values) { $nodeGuarantee.Add($n, 0) }
 
 foreach ($v in $res.slices) {
     foreach ($k in $service.Keys) { 
-    $vn = @{}
+        $vn = @{}
+        $vMin = 0
         if ($v.primary -eq $k) {
-            $v.nodeID    = $service[$k]            
+            $v.nodeID = $service[$k]
+            $vMin = (Get-SFVolume -VolumeID $v.volumeID).Qos.MinIOPS
+            $vMinSum = $vMinSum + $vMin
             [PSCustomObject]$vn = @{
                 volumeID = $v.volumeID
                 nodeID   = $v.nodeID
             }
-        $vid2nid.Add($v.volumeID,$v.nodeID)
+            $nodeGuarantee[$v.nodeID] = $vMinSum + $vMin
+            $vid2nid.Add($v.volumeID, $v.nodeID)            
         }
     }
 }
 
-Write-Host "By volume ID:"
-$vid2nid.GetEnumerator() | Sort-Object -Property Name 
+Write-Host 'Min IOPS by node ID:'
+$nodeGuarantee | Format-Table 
 
-Write-Host "By node ID:"
-$vid2nid.GetEnumerator() | Sort-Object -Property @{Expression = "Value"; Descending = $False}, @{Expression = "Name"; Descending = $False}
+Write-Host 'Volume-Node pairings by volume ID:'
+$vid2nid | Sort-Object -Property Name | Format-Table
+
+Write-Host 'Volume-Node pairings by node ID:'
+$vid2nid | Sort-Object -Property @{Expression = 'Value'; Descending = $False }, @{Expression = 'Name'; Descending = $False } | Format-Table
+
+<#
+
+SAMPLE OUTPUT FROM SOLIDFIRE 12.5 CLUSTER WITH 5 NODES
+===============================================================
+
+Min IOPS by node ID:
+
+Name                           Value
+----                           -----
+13                             193650
+10                             241150
+4                              220850
+2                              227150
+1                              208750
+
+Volume-Node pairings by volume ID:
+
+Name                           Value
+----                           -----
+262                            2
+261                            2
+260                            2
+259                            10
+258                            10
+...
+
+Volume-Node pairings by node ID:
+
+Name                           Value
+----                           -----
+92                             1
+93                             1
+122                            1
+135                            1
+255                            1
+3                              2
+4                              2
+51                             2
+54                             2
+...
+
+#>
